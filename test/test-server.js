c18728a01c01a76b73b69dd61e7437c9b931ab57
Bhavesh Gandhi <bhavgandhi@deloitte.com>
2020-06-03 16:59:56 +0530

    Test Changes

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to [TEAM NAME'S] application</h1>"));
+        chai.assert.isTrue(res.text.includes(`<h1><mark>Welcome to the <abbr title="Daring Driven Deloitte DevOps Drifters">+5D</abbr> MAGICAL WEB APP</mark></h1>`));
         return done();
       });
 
