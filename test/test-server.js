28a8563a930bd7a5b871a8630c309300b31cd465
Bhavesh Gandhi <bhavgandhi@deloitte.com>
2020-06-03 13:51:29 +0530

    UI change and Test change

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to [TEAM NAME'S] application</h1>"));
+        chai.assert.isTrue(res.text.includes(`<h1><mark>Welcome to <abbr title="Daring Driven Deloitte DevOps Drifters">DDDDD</abbr> WEB</mark></h1>`));
         return done();
       });
 
